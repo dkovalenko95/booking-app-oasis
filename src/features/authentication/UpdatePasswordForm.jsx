503c337d6bac7359df083130ba8d50268ebe480d
Dmytro <dkovalenko2425@gmail.com>
2024-01-19 14:13:38 +0200

    updateUser func for password update applied

--- src/features/authentication/UpdatePasswordForm.jsx
+++ src/features/authentication/UpdatePasswordForm.jsx
@@ -4,29 +4,31 @@ import Form from '../../ui/Form';
 import FormRow from '../../ui/FormRow';
 import Input from '../../ui/Input';
 
-import { useUpdateUser } from './useUpdateUser';
+import { useUpdateUser } from './hooks/useUpdateUser';
 
 function UpdatePasswordForm() {
   const { register, handleSubmit, formState, getValues, reset } = useForm();
   const { errors } = formState;
 
-  const { updateUser, isUpdating } = useUpdateUser();
+  const { updateCurrentUser, isUpdatingCurrUser } = useUpdateUser();
 
   function onSubmit({ password }) {
-    updateUser({ password }, { onSuccess: reset });
-  }
+    updateCurrentUser({ password }, {
+      onSuccess: () => reset(),
+    });
+  };
 
   return (
     <Form onSubmit={handleSubmit(onSubmit)}>
       <FormRow
-        label='Password (min 8 characters)'
+        label='New password (min 8 chars)'
         error={errors?.password?.message}
       >
         <Input
           type='password'
           id='password'
           autoComplete='current-password'
-          disabled={isUpdating}
+          disabled={isUpdatingCurrUser}
           {...register('password', {
             required: 'This field is required',
             minLength: {
@@ -45,7 +47,7 @@ function UpdatePasswordForm() {
           type='password'
           autoComplete='new-password'
           id='passwordConfirm'
-          disabled={isUpdating}
+          disabled={isUpdatingCurrUser}
           {...register('passwordConfirm', {
             required: 'This field is required',
             validate: (value) =>
@@ -54,10 +56,14 @@ function UpdatePasswordForm() {
         />
       </FormRow>
       <FormRow>
-        <Button onClick={reset} type='reset' variation='secondary'>
+        <Button
+          onClick={reset}
+          type='reset'
+          $variation='secondary'
+        >
           Cancel
         </Button>
-        <Button disabled={isUpdating}>Update password</Button>
+        <Button disabled={isUpdatingCurrUser}>Update password</Button>
       </FormRow>
     </Form>
   );
