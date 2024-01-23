308d0a35fbe4abe6b558fc4f2d057a17af39b0f6
Dmytro <dkovalenko2425@gmail.com>
2024-01-23 16:41:42 +0200

    empty comp if no booking added

--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -14,6 +14,7 @@ import { useCheckout } from '../check-in-out/hooks/useCheckout';
 import { useDeleteBooking } from './hooks/useDeleteBooking';
 import Modal from '../../ui/Modal';
 import ConfirmDelete from '../../ui/ConfirmDelete';
+import Empty from '../../ui/Empty';
 
 const HeadingGroup = styled.div`
   display: flex;
@@ -29,7 +30,9 @@ function BookingDetail() {
   const { checkout, isCheckingOut } = useCheckout();
   const { deleteBooking, isDeletingBooking } = useDeleteBooking();
   
-  if (isLoading) return <Spinner />
+  if (isLoading) return <Spinner />;
+
+  if (!booking) return <Empty resource='booking' />;
   
   const { status, id: bookingId } = booking;
 
